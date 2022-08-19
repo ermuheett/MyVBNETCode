5ce90c4b305db3a5c84a057ca310fe6e727fc988
Muheet Ahmed Butt <111588538+ermuheett@users.noreply.github.com>
2022-08-19 22:01:54 +0530

    Add files via upload

---
+++ WindowsAppExamStatus/WindowsAppExamStatus/FrmStatus.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmStatus
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButnAssignAttendance = New System.Windows.Forms.Button()
+        Me.butnAccessment = New System.Windows.Forms.Button()
+        Me.butnInit = New System.Windows.Forms.Button()
+        Me.ButnCheckRemaining = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButnAssignAttendance
+        '
+        Me.ButnAssignAttendance.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButnAssignAttendance.Location = New System.Drawing.Point(186, 130)
+        Me.ButnAssignAttendance.Name = "ButnAssignAttendance"
+        Me.ButnAssignAttendance.Size = New System.Drawing.Size(495, 73)
+        Me.ButnAssignAttendance.TabIndex = 0
+        Me.ButnAssignAttendance.Text = "Assign Attendance as per Email"
+        Me.ButnAssignAttendance.UseVisualStyleBackColor = True
+        '
+        'butnAccessment
+        '
+        Me.butnAccessment.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.butnAccessment.Location = New System.Drawing.Point(186, 231)
+        Me.butnAccessment.Name = "butnAccessment"
+        Me.butnAccessment.Size = New System.Drawing.Size(495, 73)
+        Me.butnAccessment.TabIndex = 1
+        Me.butnAccessment.Text = "Assign Assessment"
+        Me.butnAccessment.UseVisualStyleBackColor = True
+        '
+        'butnInit
+        '
+        Me.butnInit.Enabled = False
+        Me.butnInit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.butnInit.Location = New System.Drawing.Point(186, 34)
+        Me.butnInit.Name = "butnInit"
+        Me.butnInit.Size = New System.Drawing.Size(495, 73)
+        Me.butnInit.TabIndex = 2
+        Me.butnInit.Text = "Intialization"
+        Me.butnInit.UseVisualStyleBackColor = True
+        '
+        'ButnCheckRemaining
+        '
+        Me.ButnCheckRemaining.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButnCheckRemaining.Location = New System.Drawing.Point(186, 339)
+        Me.ButnCheckRemaining.Name = "ButnCheckRemaining"
+        Me.ButnCheckRemaining.Size = New System.Drawing.Size(495, 73)
+        Me.ButnCheckRemaining.TabIndex = 3
+        Me.ButnCheckRemaining.Text = "Compare Certificate with Registration"
+        Me.ButnCheckRemaining.UseVisualStyleBackColor = True
+        '
+        'FrmStatus
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(886, 656)
+        Me.Controls.Add(Me.ButnCheckRemaining)
+        Me.Controls.Add(Me.butnInit)
+        Me.Controls.Add(Me.butnAccessment)
+        Me.Controls.Add(Me.ButnAssignAttendance)
+        Me.Name = "FrmStatus"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ButnAssignAttendance As Button
+    Friend WithEvents butnAccessment As Button
+    Friend WithEvents butnInit As Button
+    Friend WithEvents ButnCheckRemaining As Button
+End Class
---
+++ WindowsAppExamStatus/WindowsAppExamStatus/FrmStatus.vb
@@ -0,0 +1,229 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+
+
+
+
+
+Public Class FrmStatus
+
+    Dim CNLTS As New SqlConnection("Data Source=(local);Initial Catalog=LangTransSkills;Integrated Security=True;MultipleActiveResultSets=True;Connect Timeout=1000")
+
+
+
+    Private Sub ButnAssignAttendance_Click(sender As Object, e As EventArgs) Handles ButnAssignAttendance.Click
+
+
+
+        'Select all from hte table. 
+
+
+        Dim rsCNTSUpdate As SqlCommand
+        Dim rsCNTSSelect As SqlCommand
+
+        Dim rsPending As SqlCommand
+
+
+        Dim rsAtt As New SqlCommand("Select * from  Attendance order by sno ", CNLTS)
+
+        Dim intCnt As Integer = 0
+
+
+        CNLTS.Open()
+
+
+
+        Dim rdrAtt As SqlDataReader = rsAtt.ExecuteReader
+
+        While rdrAtt.Read
+
+            If rdrAtt("sno") >= 0 Then
+
+
+
+                Me.Text = rdrAtt("sno").ToString
+
+                'Check for Selection
+
+                rsCNTSSelect = New SqlCommand("select count(*) from pendinglist where emailid = '" & rdrAtt("emailID") & "'", CNLTS)
+                intCnt = rsCNTSSelect.ExecuteScalar()
+
+
+
+                If intCnt > 0 Then
+                    rsCNTSUpdate = New SqlCommand("Update pendinglist set noofclasses = noofclasses + 1 where emailid = '" & rdrAtt("emailID") & "'", CNLTS)
+                    rsCNTSUpdate.ExecuteNonQuery()
+                Else
+
+                    rsPending = New SqlCommand("insert into Pending values (" & rdrAtt("sno") & ",'" & rdrAtt("emailid") & "','" & rdrAtt("name") & "')", CNLTS)
+                    rsPending.ExecuteNonQuery()
+
+
+                End If
+
+
+
+            End If
+
+        End While
+
+
+
+        CNLTS.Close()
+
+
+        MsgBox("Done Successfully..!!!")
+
+
+
+    End Sub
+
+    Private Sub butnAccessment_Click(sender As Object, e As EventArgs) Handles butnAccessment.Click
+
+        ' Assign Assignment. 
+        'Traverse Assignment data Table. 
+
+        Dim cmdAssess As New SqlCommand("Select * from EXAM_PHASE_B ", CNLTS)
+        Dim rdrAccess As SqlDataReader
+
+
+        Dim rsRegistration As SqlCommand
+
+        Dim rsRegSelect As SqlCommand
+
+        Dim intCntr As Integer = 0
+
+
+
+        CNLTS.Open()
+
+        rdrAccess = cmdAssess.ExecuteReader
+
+        While rdrAccess.Read
+
+            'Check for record in registration table..
+
+            rsRegSelect = New SqlCommand("Select count(*) from pendinglist  where emailid  = '" & rdrAccess("EMAILID") & "'", CNLTS)
+            intCntr = rsRegSelect.ExecuteScalar()
+
+
+
+            If intCntr > 0 Then
+
+                rsRegistration = New SqlCommand("update pendinglist set ex_status = 'A' where emailid = '" & rdrAccess("EmailID") & "'", CNLTS)
+                rsRegistration.ExecuteNonQuery()
+
+            End If
+
+        End While
+
+
+
+
+        CNLTS.Close()
+
+
+        MsgBox("Done Successfully..!!!")
+
+
+
+    End Sub
+
+    Private Sub butnInit_Click(sender As Object, e As EventArgs) Handles butnInit.Click
+
+        'Table Intitialation Code.. 2 tables... 
+        'Delete Classes
+
+        Dim rsUpdateClass As New SqlCommand("update pendinglist set noofclasses = 0, ex_status = 'N'", CNLTS)
+        'Delete Status 
+
+
+        Dim cmdDeletePending As New SqlCommand("Delete from Pending", CNLTS)
+
+
+
+        CNLTS.Open()
+
+        cmdDeletePending.ExecuteNonQuery()
+        rsUpdateClass.ExecuteNonQuery()
+
+        CNLTS.Close()
+
+
+        MsgBox("Initialization is Complete..!!!!")
+
+    End Sub
+
+    Private Sub ButnCheckRemaining_Click(sender As Object, e As EventArgs) Handles ButnCheckRemaining.Click
+
+
+        'Traverse Through Attendance..
+
+
+        Dim cmdAttendance As New SqlCommand("Select * from  Attendance order by sno ", CNLTS)
+
+        Dim cmdPendingSelect
+
+
+        Dim intCnt As Integer = 0
+
+
+        CNLTS.Open()
+
+
+
+        Dim rdrAtt As SqlDataReader = cmdAttendance.ExecuteReader
+
+        While rdrAtt.Read
+
+            If rdrAtt("sno") >= 0 Then
+
+
+
+                Me.Text = rdrAtt("sno").ToString
+
+                'Check for Selection
+
+                cmdCNTSSelect = New SqlCommand("select count(*) from pendinglist where emailid = '" & rdrAtt("emailID") & "'", CNLTS)
+                intCnt = rsCNTSSelect.ExecuteScalar()
+
+
+
+                If intCnt > 0 Then
+                    rsCNTSUpdate = New SqlCommand("Update pendinglist set noofclasses = noofclasses + 1 where emailid = '" & rdrAtt("emailID") & "'", CNLTS)
+                    rsCNTSUpdate.ExecuteNonQuery()
+                Else
+
+                    rsPending = New SqlCommand("insert into Pending values (" & rdrAtt("sno") & ",'" & rdrAtt("emailid") & "','" & rdrAtt("name") & "')", CNLTS)
+                    rsPending.ExecuteNonQuery()
+
+
+                End If
+
+
+
+            End If
+
+        End While
+
+
+
+        CNLTS.Close()
+
+
+        MsgBox("Done Successfully..!!!")
+
+
+
+
+
+        'Ignore if Printed 
+
+        'Store rest in Pending but only one Occurance.. 
+
+        'send some Seperate Mails for confirmation 
+
+
+
+    End Sub
+End Class
