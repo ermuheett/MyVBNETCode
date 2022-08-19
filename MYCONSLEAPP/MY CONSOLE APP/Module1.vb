04a6b1689cc259ad4e833dbb624e85c5595e1c6e
Muheet Ahmed Butt <111588538+ermuheett@users.noreply.github.com>
2022-08-19 21:49:05 +0530

    Add files via upload

---
+++ MYCONSLEAPP/MY CONSOLE APP/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.WriteLine(" Enter Your Favorite Application")
+
+        Dim Language As String
+
+        Language = Console.ReadLine()
+
+        If Language = "Visual Basic" Then
+
+            Console.WriteLine("You are winner")
+
+        Else
+
+            Console.WriteLine(" You are looser")
+
+        End If
+
+        Console.WriteLine()
+        Console.WriteLine()
+
+        Console.WriteLine("Press Any Key to Exit")
+
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
